a337206eb671aa0ee8444d7a3b45b9d4a6db5149
codeCamper07 <dommaraju.bhargav007@gmail.com>
2023-06-27 17:49:39 +0530

    new

---
+++ src/components/Services-card/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+const ServicesCard = (props) => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+export default ServicesCard
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,13 +1,44 @@
 import { Component } from 'react'
 import './index.css'
 import Header from '../Header'
+import { Button } from '@mui/material'
+
+const ServicesList = [
+  {
+    name: 'Application Security',
+    description: 'Content',
+  },
+  {
+    name: 'Network Security',
+    description: 'Content',
+  },
+  {
+    name: 'CERT-In Audit Services',
+    description: 'Content',
+  },
+]
 
 class Services extends Component {
   render() {
     return (
       <div>
         <Header />
-        <h1>Services</h1>
+        <div className='services-section'>
+          <div className='main-banner'>
+            <h1 className='heading'>Our Services</h1>
+            <p>
+              content content content content content content content content
+              content content content content content content content content
+              content content content content content content content content
+              content content content content content content content content
+              content content content content content content content
+            </p>
+            <div>
+              <Button variant='contained'>Contact us</Button>
+            </div>
+          </div>
+          <div className='services-offered-section'></div>
+        </div>
       </div>
     )
   }
