dfecf8fa2ea05ae84ac12025b41cdf06b5021883
codeCamper07 <dommaraju.bhargav007@gmail.com>
2023-06-30 16:26:25 +0530

    updated

--- src/components/Trainings/index.js
+++ src/components/Trainings/index.js
@@ -1,13 +1,17 @@
 import { Component } from 'react'
 import './index.css'
 import Header from '../Header'
+import Footer from '../Footer'
 
 class Trainings extends Component {
   render() {
     return (
       <div>
         <Header />
-        <h1>Trainings</h1>
+        <div className='trainings-container'>
+          <h1>Trainings</h1>
+        </div>
+        <Footer />
       </div>
     )
   }
