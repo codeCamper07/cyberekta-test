4f9834b553539d6cdfc63b00cba70e9017093fb3
codeCamper07 <dommaraju.bhargav007@gmail.com>
2023-07-23 09:16:47 +0530

    new

--- src/components/T&C/index.js
+++ src/components/T&C/index.js
@@ -6,7 +6,203 @@ const TermsAndConditions = () => {
   return (
     <div>
       <Header />
-      <h1>Terms And Conditions</h1>
+      <h1 style={{ 'text-align': 'center' }}>Terms And Conditions</h1>
+      <div style={{ padding: '10px', fontSize: '18px' }}>
+        <h3>Welcome</h3>
+        <p>
+          These Cyberekta Web Site Terms of Use ("Terms") govern all
+          Cyberekta.com web pages ("Site"). By accessing, visiting, or otherwise
+          using the Site, you agree to be bound by the Terms.
+        </p>
+        <h3>Your Use of Our Site</h3>
+        <p>
+          Cyberekta is committed to providing a safe and positive experience to
+          all users on our Site. To help us do that, we eed you to follow a few
+          basic rules when you’re here. Don’t worry, it’s not very complicated.
+        </p>
+        <p>
+          When using our Site, you must comply with all applicable laws,
+          including federal, state, and local laws, the laws of your
+          jurisdiction, and laws regarding the transmission of technical data.
+        </p>
+        <h3>You also agree not to:</h3>
+        <ul>
+          <li>
+            Display, send, receive, or store obscene or inappropriate content.
+          </li>
+          <li>
+            Threaten, harass, stalk, defame, or defraud any person or entity.
+          </li>
+          <li>
+            Violate copyright, trademark, or other intellectual property laws.
+          </li>
+          <li>
+            Advertise, promote, endorse, or market, directly or indirectly, any
+            third party commercial products, services, solutions, or other
+            technologies.
+          </li>
+          <li>
+            Attempt to collect, store, or publish personally identifiable
+            information (a) without the owner’s knowledge and consent or (b) of
+            a minor under the age of thirteen (13) in any circumstance.
+          </li>
+          <li>
+            Distribute unwanted, unsolicited, or harassing mass email or other
+            messages, promotions, advertising, or solicitations (“spam”).
+          </li>
+          <li>
+            Send deceptive or false source-identifying information, including
+            “spoofing” or “phishing.”
+          </li>
+          <li>
+            Access or use any application, system, service, tool, data, account,
+            network, or content without authorization or for unintended
+            purposes.
+          </li>
+          <li>
+            Disable, disrupt, circumvent, interfere with, or otherwise violate
+            the security of the Site.
+          </li>
+          <li>
+            Attack, abuse, interfere with, intercept, disrupt, or exploit any
+            users, systems, or services, regardless of how accomplished and
+            notwithstanding anything to the contrary in these Terms, including
+            but not limited to Denial of Service (DoS), monitoring, crawling,
+            spamming, using bots or scripts, or distributing malware (such as
+            viruses, Trojan horses, worms, spyware, or adware).
+          </li>
+          <li>
+            Engage in or promote any illegal or criminal activity such as child
+            pornography, gambling, or piracy.
+          </li>
+          <li>
+            Authorize, permit, enable, induce, or encourage any third party to
+            do any of the above. If you violate these Terms, your access to the
+            Site may be terminated immediately and without notice. In other
+            words, just be respectful and use the Site for its intended purpose.
+          </li>
+        </ul>
+        <h3>Our Site Content</h3>
+        <p>
+          Our Site contains content owned, operated, licensed, and/or controlled
+          by Cyberekta that is protected by copyright, trademark, trade secret,
+          or other proprietary rights ("Site Content"). Cyberekta or its
+          licensors retain all rights in such Site Content. Cyberekta grants you
+          a limited, revocable, non-sublicensable right to view the Site Content
+          solely for your internal use of the Site. Additionally, certain Site
+          Content may include, but is not limited to, icons, photographs, white
+          papers, product documentation, technology overviews, and
+          implementation guides. These materials may be used under limited
+          circumstances in strict accordance with our Copyright Use
+          Policies and Guidelines.
+        </p>
+        <h3>Your Content</h3>
+        <p>
+          We do not claim any ownership rights in any text, files, images,
+          photos, video, sounds, software, works of authorship, or other
+          materials that you upload or otherwise provide to our Site
+          (collectively, "User Content"). However, you grant to Cisco a
+          worldwide, royalty-free, sublicensable, perpetual, irrevocable license
+          to use, modify, publicly perform, publicly display, reproduce, and
+          distribute your User Content for the limited purpose of providing the
+          Site and all related services. You also agree that any other user of
+          this Site may access, view, store, and reproduce your User Content for
+          such user's personal use or otherwise in connection with use of the
+          Site.
+        </p>
+        <p>
+          You may intend for some of your User Content to be generally available
+          as Software Content on our Site. In those cases, in addition to the
+          permissions that you grant above, you must also specify a license that
+          provides other users with sufficient rights to use your work as
+          intended, such as an open source or Creative Commons license. For
+          guidance on how to choose an appropriate free software license, this
+          is a helpful third party resource:
+          <a
+            style={{ textDecoration: 'underline', color: 'blue' }}
+            href='https://choosealicense.com/'
+            target='_blank'
+            rel='noopener noreferrer'>
+             https://choosealicense.com/
+          </a>
+        </p>
+        <p>
+          You cannot post, modify, distribute, or reproduce in any way
+          copyrighted or other proprietary materials without obtaining the prior
+          written consent of the copyright owner of such materials. We may
+          terminate an account and/or deny access to any user who is alleged to
+          have infringed the copyright or proprietary rights of another.
+        </p>
+        <p>
+          All User Content is considered nonconfidential and public. Please do
+          not submit any confidential or private information.
+        </p>
+        <p>
+          If you provide us with any feedback or ideas, you authorize Cisco to
+          use your feedback for any purpose, without any restriction or
+          limitation.
+        </p>
+        <p>
+          If you believe that your copyright or other rights have been
+          infringed, please provide our Designated Agent written notice with the
+          following information:
+        </p>
+        <ul>
+          <li>
+            An electronic or physical signature of the person authorized to act
+            on behalf of the owner of the copyright or other interest
+          </li>
+          <li>
+            A description of the copyrighted work or other work that you claim
+            has been infringed
+          </li>
+          <li>
+            A description of where the material that you claim is infringing is
+            located on our sites, networks or other repositories
+          </li>
+          <li>Your address, telephone number, and email address</li>
+          <li>
+            A written statement by you that you have a good faith belief that
+            the disputed use is not authorized by the copyright owner, its
+            agent, or the law
+          </li>
+          <li>
+            A statement by you, made under penalty of perjury, that the above
+            information in your notice is accurate and that you are the
+            copyright owner or authorized to act on the copyright owner's behalf
+          </li>
+        </ul>
+        <h3>About Our Terms</h3>
+        <p>
+          We reserve the right to change these Terms from time to time at our
+          sole discretion. Please check this page periodically for any updates
+        </p>
+        <p>
+          These Terms are governed by the laws of the State of Telangana, India.
+        </p>
+        <p>
+          If any provision of these Terms is found to be invalid or
+          unenforceable, that provision will be struck, and the remaining
+          provisions will remain in full effect.
+        </p>
+        <p>
+          If you or others violate these Terms and we take no immediate action,
+          this in no way limits or waives our rights, such as our right to act
+          in the future or in similar situations.
+        </p>
+        <p>
+          These terms were last updated on 08/09/2022 and are effective
+          immediately. Thank You for Visiting.
+        </p>
+        <h3>Further Enquiries:</h3>
+        <p>
+          You can reach out at <br />
+          Address: Plot no.6 & 7, Road no.2, Green Hills Colony, Hyderabad,
+          500035.
+          <br />
+          Email: info@cyberekta.com
+        </p>
+      </div>
       <Footer />
     </div>
   )
